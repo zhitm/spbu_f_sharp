a26c7d301b5314faed3cc7338feb4c8567acab3f
zhitm <zhtinuhina.maria@gmail.com>
2023-05-26 00:40:46 +0300

    added tests on simulate + fixed

--- LocalNetwork/net.fs
+++ LocalNetwork/net.fs
@@ -17,15 +17,15 @@ let probabilities =
          Hard, 0]
 
 
-let shouldIGetInfected (probability: float) =
-    (float (Random().Next(1, 100))) <= probability * (float 100)
-
-
 type Computer(os: OS, ip: string) =
-
-    member this.OS = os
     member this.ip = ip
+    member this.OS = os
     member this.probability = probabilities[os]
+    member this.canBecomeInfected = this.probability <> 0
+    static member private rand = Random()
+
+    member this.shouldIGetInfected =
+        (float (Computer.rand.Next(1, 100))) <= this.probability * (float 100)
 
 
 let rec makeStep (infected: List<Computer>) (neighbours: List<Computer * Computer>) (newInfected: List<Computer>) =
@@ -33,35 +33,37 @@ let rec makeStep (infected: List<Computer>) (neighbours: List<Computer * Compute
         newInfected
     else
         match neighbours.Head with
-        | a, b when
-            List.contains a infected
-            && not (List.contains b infected)
-            && (shouldIGetInfected b.probability)
-            ->
-            makeStep infected neighbours.Tail (b :: newInfected)
+        | a, b
         | b, a when
             List.contains a infected
             && not (List.contains b infected)
-            && (shouldIGetInfected b.probability)
+            && (b.shouldIGetInfected)
             ->
             makeStep infected neighbours.Tail (b :: newInfected)
-        | _, _ -> makeStep infected neighbours.Tail newInfected
+
+        | _ -> makeStep infected neighbours.Tail newInfected
 
 let rec printElements (ls: List<Computer>) =
-    if ls = [] then
-        ()
-    else
+    if not ls.IsEmpty then
         printfn $"{ls.Head.ip}"
         printElements ls.Tail
 
+let getCntCanBeInfected computers =
+    List.sumBy (fun (comp: Computer) -> if comp.canBecomeInfected then 1 else 0) computers
+
+let union left right =
+    List.append left right |> Seq.distinct |> List.ofSeq
+
 let rec simulate (infected: List<Computer>) (neighbours: List<Computer * Computer>) step listOfComputers =
     System.Threading.Thread.Sleep(100)
-    let currentInfected = infected @ (makeStep infected neighbours [])
+    let currentInfected = union infected (makeStep infected neighbours [])
     printfn $"infected on {step}: "
     printElements currentInfected
 
-    if List.length currentInfected <> List.length listOfComputers then
+    if List.length infected <> getCntCanBeInfected listOfComputers then
         simulate currentInfected neighbours (step + 1) listOfComputers
+    else
+        infected
 
 
 let c1 = Computer(Linux, "1")
@@ -71,6 +73,6 @@ let c4 = Computer(MacOs, "4")
 let c5 = Computer(Windows, "5")
 let c6 = Computer(MacOs, "6")
 
-let neighbours = [ c1, c2; c1, c6; c2, c3; c3, c4; c4, c5 ]
+let neighbours = [ (c1, c2); (c1, c6); (c2, c3); (c3, c4); (c4, c5) ]
 
-simulate [ c1 ] neighbours 1 [ c1; c2; c3; c4; c5; c6]
+simulate [ c1 ] neighbours 1 [ c1; c2; c3; c4; c5; c6 ]
--- TestNetwork/UnitTest1.fs
+++ TestNetwork/UnitTest1.fs
@@ -4,11 +4,30 @@ open NUnit.Framework
 open FsUnit
 open LocalNetwork
 
+[<Test>]
+let ``Test on virus-safe PC`` () =
+    let c = Computer(Hard, "1")
+    c.shouldIGetInfected |> should equal false
 
 [<Test>]
-let ``Test on virus-safe PC``() =
-    shouldIGetInfected(probabilities[Hard]) |> should equal false
-    
+let ``Test on virus-unsafe PC`` () =
+    let c = Computer(Weak, "1")
+    c.shouldIGetInfected |> should equal true
+
 [<Test>]
-let ``Test on virus-unsafe PC``() =
-    shouldIGetInfected(probabilities[Weak]) |> should equal true
+let ``Test simulation on weak`` () =
+    let c1 = Computer(Weak, "1")
+    let c2 = Computer(Weak, "2")
+    let c3 = Computer(Weak, "3")
+    let neighbours = [ (c1, c2); (c1, c2); (c1, c3) ]
+    let infected = simulate [ c1 ] neighbours 1 [ c1; c2; c3 ]
+    infected |> should equal [c1; c3; c2]
+
+[<Test>]
+let ``Test simulation on hard`` () =
+    let c1 = Computer(Hard, "1")
+    let c2 = Computer(Hard, "2")
+    let c3 = Computer(Hard, "3")
+    let neighbours = [ (c1, c2); (c1, c2); (c1, c3) ]
+    let infected = simulate [ c1 ] neighbours 1 [ c1; c2; c3 ]
+    infected |> should equal []
