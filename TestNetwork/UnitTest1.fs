bbbd5938dd5416cb96e4a0591967d79bd6186c3d
zhitm <zhtinuhina.maria@gmail.com>
2023-05-26 00:48:06 +0300

    fixed test on hard pc

--- TestNetwork/UnitTest1.fs
+++ TestNetwork/UnitTest1.fs
@@ -25,9 +25,9 @@ let ``Test simulation on weak`` () =
 
 [<Test>]
 let ``Test simulation on hard`` () =
-    let c1 = Computer(Hard, "1")
+    let c1 = Computer(Weak, "1")
     let c2 = Computer(Hard, "2")
     let c3 = Computer(Hard, "3")
     let neighbours = [ (c1, c2); (c1, c2); (c1, c3) ]
     let infected = simulate [ c1 ] neighbours 1 [ c1; c2; c3 ]
-    infected |> should equal []
+    infected |> should equal [c1]
